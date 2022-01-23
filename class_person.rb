297c7710bb8feee2181f2acc26cd3aedb68f1259
adrianlm2 <adrianl0@chicagobooth.edu>
2022-01-23 16:37:26 +0000

    attempt

--- class_person.rb
+++ class_person.rb
@@ -7,5 +7,17 @@
 #  - An instance method, age, that calculates the number of years between today and birthdate
 
 class Person
-  
+  attr_accessor :first_name
+  attr_accessor :last_name
+  attr_accessor :birthdate
+  def full_name
+    return self.first_name + " " + self.last_name
+  end
+  def age_calculator
+    dob = Date.parse(self.birthdate)
+    now = Date.today
+    age_in_days = now - dob
+    age_in_years = age_in_days / 365
+
+    return age_in_years.to_i
 end
